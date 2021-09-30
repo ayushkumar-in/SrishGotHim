a32f25987b6e031f34d38e35994ecd32bdfbc770
Ayush <ayush.kumar20@yahoo.in>
2021-09-30 12:03:10 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-window.addEventListener("load", function () {
-  setTimeout(function () {
-    document.getElementById('loaderDiv').style.display = 'none';
-  }, 1000);
-})
+// window.addEventListener("load", function () {
+//   setTimeout(function () {
+//     document.getElementById('loaderDiv').style.display = 'none';
+//   }, 1000);
+// })
 
 function navigateTo(destination) {
   window.location = destination;
