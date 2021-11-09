b068015dbae1afb4d681124d941691d9dd88f87e
Ayush <ayush.kumar20@yahoo.in>
2021-11-10 00:36:13 +0530

    update

--- js/home/index.js
+++ js/home/index.js
@@ -1,8 +1,9 @@
-window.addEventListener("mousemove", parallaxBack);
-function parallaxBack(e) {
-    document.querySelectorAll('.parallax').forEach(parallax => {
-        const speed = parallax.getAttribute('speed');
-        const x = (window.innerWidth - e.pageX * speed) / 300
-        parallax.style.transform = `translateX(${x}px)`;
-    })
-}
+// window.addEventListener("mousemove", parallaxBack);
+// function parallaxBack(e) {
+//     document.querySelectorAll('.parallax').forEach(parallax => {
+//         const speed = parallax.getAttribute('speed');
+//         const x = (window.innerWidth - e.pageX * speed) / 300;
+//         const y =(window.innerHeight - e.pageY * speed) / 200;
+//         parallax.style.transform = `translate(${x}px,${y}px)`;
+//     })
+// }
